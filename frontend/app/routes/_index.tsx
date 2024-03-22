693da50cd6267dffddea11c8b1c7869b429ec314
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-22 11:50:21 -0300

    feat: adiciona link para o remix

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,5 +1,12 @@
+import { Link } from "@remix-run/react"
 import { Button } from "~/components/ui/button"
 
 export default function Index() {
-    return <Button>Welcome to Remix</Button>
+    return (
+        <Button variant="link" asChild>
+            <Link to="https://remix.run" target="_blank" rel="noreferrer">
+                Welcome to Remix
+            </Link>
+        </Button>
+    )
 }
