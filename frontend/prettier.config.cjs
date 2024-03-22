b7926fcc794b8383c2b6e5a4ddf010f53adde0ea
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-22 01:50:47 -0300

    chore: adiciona o prettier

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    semi: false,
+    singleQuote: false,
+    bracketSameLine: true,
+    trailingComma: "all",
+    tabWidth: 4,
+    printWidth: 100,
+    quoteProps: "consistent",
+}
