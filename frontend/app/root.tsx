879c561f3eec7a55aa1898cdf03bad7cc6b0dde9
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-22 01:54:21 -0300

    style: passa o lint nos arquivos

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,29 +1,32 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/node"
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
+
+export const meta: MetaFunction = () => [
+    {
+        charSet: "utf-8",
+    },
+    {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+    },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pt-br">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    )
 }
 
 export default function App() {
-  return <Outlet />;
+    return <Outlet />
 }
