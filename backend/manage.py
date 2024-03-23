8228a15c4225ecfe3894199e1fbc71f304f95fd7
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-23 02:15:06 -0300

    feat: conecta backend com banco de dados local

--- backend/manage.py
+++ backend/manage.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vivaboavista.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
