7c552d9d21a92454b76a16b67ea4cc536d48ebbd
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-03-29 16:56:56 -0300

    refactor: app -> src

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,24 @@
 import { vitePlugin as remix } from "@remix-run/dev"
 import { installGlobals } from "@remix-run/node"
+import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 installGlobals()
 
 export default defineConfig({
-    plugins: [remix(), tsconfigPaths()],
+    plugins: [
+        remix({
+            appDirectory: fileURLToPath(new URL("./src", import.meta.url)),
+        }),
+    ],
     server: {
         port: 3000,
         host: true,
+    },
+    resolve: {
+        alias: {
+            "~": fileURLToPath(new URL("./src", import.meta.url)),
+            "~~": fileURLToPath(new URL("./", import.meta.url)),
+        },
     },
 })
